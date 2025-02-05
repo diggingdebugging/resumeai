897f4e11c41b85954fef1cc12f0f1f8653ccec48
yujin <yujin3289@naver.com>
2025-02-05 21:37:29 +0900

    스프링 프로젝트 생성 및 build.gradle파일 자바 버전 설정 변경

---
+++ src/main/java/com/yujin/resumeai/resumeai/ResumeaiApplication.java
@@ -0,0 +1,13 @@
+package com.yujin.resumeai.resumeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeaiApplication.class, args);
+	}
+
+}
